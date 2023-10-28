7fcf31f14a6e21906520f846c1dcde9afd3729c6
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2023-10-28 22:20:31 +0300

    Добавлен admin класс для ParentalRelation model

--- menu_drawer/admin.py
+++ menu_drawer/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from menu_drawer.forms import ParentalRelationForm
 from menu_drawer.models import ParentalRelation, Menu, MenuItem
 
 
@@ -9,6 +10,10 @@ class MenuItemAdmin(admin.ModelAdmin):
     }
 
 
-admin.site.register(ParentalRelation)
+class ParentalRelationAdmin(admin.ModelAdmin):
+    form = ParentalRelationForm
+
+
+admin.site.register(ParentalRelation, ParentalRelationAdmin)
 admin.site.register(Menu)
 admin.site.register(MenuItem, MenuItemAdmin)
